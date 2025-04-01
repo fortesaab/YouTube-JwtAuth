e3f8d75009120caf014264f2dbfe5aa11c58dac5
fortesaab <fortesa.abdullahuu@gmail.com>
2025-04-01 06:37:17 -0700

    implement basic auth in AuthController

--- Core/Dtos/LoginDto.cs
+++ Core/Dtos/LoginDto.cs
@@ -7,9 +7,6 @@ namespace JwtAuth.Core.Dtos
         [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
-        public string Email { get; set; }
-
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
     }
